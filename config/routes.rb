c37d83893877953f1ec5d2f463d3983f0bee5660
Wesley Beck <wes.beck21@outlook.com>
2022-07-31 20:16:01 -0600

    [Routes] Add routes for order, toppings, items. Add controller for orders
    Add best_customers action to controller actions, begin stubbing out actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      # Start Resources
+      resources :orders do
+        collection do
+          get :best_customers
+        end
+      end
+      resources :items
+      resources :toppings
+      # End Resources
+    end
+  end
 end

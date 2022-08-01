9ebb9e49edda0874845b1c444225664161eb1739
Wesley Beck <wes.beck21@outlook.com>
2022-07-31 21:12:11 -0600

    [Serializer, OrdersController] Update controller, add order serializer
    Add OrderSerializer for json formatting using jsonapi-serializer gem
    Update Orders Controller to make first api request

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::OrdersController < ApplicationController
   def index
     # Retrieve all orders figure out how to incorporate lazy load
+    @orders = Order.order(:created_at).limit(50)
+    render json: OrderSerializer.new(@orders).serializable_hash, status: :ok
   end
 
   def best_customers
     @orders = Order.best_customers
-    render json: OrdersSerializer.new(@orders).serializable_hash
+    render json: OrdersSerializer.new(@orders).serializable_hash, status: :ok
   end
 end

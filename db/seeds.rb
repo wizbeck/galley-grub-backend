409f52be8199a93eeca917308d62a164ba2b5113
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-09-04 09:58:52 -0600

    [SEEDS] WIP stub out seed file execution
    Clear db functions
    Add ENTREES constant holding Item names for building base items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# This seed file needs to do the following:
+#   1. Create items that match with the Krusty Krab Galley Grub menu
+#   2. Create realistic toppings that coincide with certain menu_items, set category
+#     - check the relationship with category of an item, vs the category type on a topping.
+#     - We might need to create a category object, which holds the name and some sort of id/integer to distinguish 
+#       the category
+#
+
+# Destroy user related records
+Order.destroy_all if Order.exists?
+OrderItem.destroy_all if OrderItem.exists?
+OrderItemTopping.destroy_all if OrderItemToping.exists?
+
+# Re seed the items and toppings
+Item.destroy_all if Item.exists?
+Topping.destroy_all if Topping.exists?
+ItemTopping.destroy_all if ItemTopping.exists?
+
+# Build the items and toppings
+ENTREES = [
+  'Krabby Patty',
+  'Double Krabby Patty',
+  'Triple Krabby Patty',
+
+]
+
+

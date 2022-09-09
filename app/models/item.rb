ff0d0c90cd41e5f9ebb379a65440184bf12152e4
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-09-08 21:39:40 -0600

    [Relations] Add missing relation macro for items-item_toppings

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   # Relationships
   belongs_to :order
+  has_many :item_toppings
 
   # Enum integers for item types
   enum item_type: { 'entree' => 1, 'side' => 2, 'drink' => 3 }
@@ -9,5 +10,8 @@ class Item < ApplicationRecord
   scope :entrees, -> { where(type: :entree) }
   scope :drinks, -> { where(type: :drink) }
   scope :sides, -> { where(type: :side) }
+
+  # item_toppings are join table records associated already existing toppings to an item that can be reused between items
+  accepts_nested_attributes_for :item_toppings
   # Methods
 end

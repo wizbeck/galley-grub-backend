cd179f32a6ac341d224e24b026bb6cb537ee7cd8
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-09-10 15:11:51 -0600

    [Seed Data] Fix various seed typos, add full menu items and toppings seed data, and item_topping associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@
 # Destroy user related records
 Order.destroy_all if Order.exists?
 OrderItem.destroy_all if OrderItem.exists?
-OrderItemTopping.destroy_all if OrderItemToping.exists?
+OrderItemTopping.destroy_all if OrderItemTopping.exists?
 
 # Re seed the items and toppings
 Item.destroy_all if Item.exists?
@@ -28,19 +28,54 @@ ItemTopping.destroy_all if ItemTopping.exists?
 # Build the items and toppings
 # we can clone a order item, with certain toppings, and additional items as a combo
 
-# Create Entrees
+# Items
 items = [
   { name: 'Krabby Patty', price: 1.25, item_type: 'entree' },
   { name: 'Double Krabby Patty', price: 2.00, item_type: 'entree' },
   { name: 'Triple Krabby Patty', price: 3.00, item_type: 'entree' },
-  { name: 'Coral Bits', item_type: 'side' }, # sides default to 0. small, medium, large toppings are prices
+  { name: 'Coral Bits', price: 1.00, item_type: 'side' },
   { name: 'Kelp Rings', price: 1.50, item_type: 'side' },
   { name: 'Kelp Shake', price: 2.00, item_type: 'drink' },
-  { name: 'Seafoam Soda', item_type: 'drink' } # Drink item, with small, med, large toppings
+  { name: 'Seafoam Soda', price: 1.00, item_type: 'drink' },
+  { name: 'Sailors Surprise', price: 3.00, item_type: 'entree' },
+  { name: 'Footlong', price: 2.00, item_type: 'entree' },
+  { name: 'Salty Sea Dog', price: 1.25, item_type: 'entree' },
+  { name: 'Golden Loaf', price: 2.00, item_type: 'entree' }
 ]
 
+# Create Items
+Item.create(items)
+
+# Toppings
+toppings = [
+  { name: 'small' },
+  { name: 'medium', price: 0.25 },
+  { name: 'large', price: 0.50 },
+  { name: 'sauce', price: 0.50 },
+  { name: 'salty sauce', price: 0.50 },
+  { name: 'sea cheese', price: 0.25 }
+]
+
+# Create Toppings
+Topping.create(toppings)
+
+# Associate toppings to items via item_toppings
+entree_toppings = Topping.where(name: ['sauce', 'salty sauce', 'sea cheese']).map { |t| { topping_id: t.id } }
+
+# Associate toppings to  entree items
+entrees = Item.entree
+entrees.each { |e| e.item_toppings.create(entree_toppings) }
+
+# Associate toppings to sides and drinks
+
+sides_drinks = Item.where(item_type: %w[side drink])
+size_toppings = Topping.where(name: %w[small medium large]).map { |t| { topping_id: t.id } }
+sides_drinks.each { |sd| sd.item_toppings.create(size_toppings) }
+
+
 # TODO: Figure out how to build combos into a method of creating OrderItem, with OrderItem Toppings.
- # Maybe send messages or api requests to return the specific orderitems with toppings prebuilt
+  # Maybe send messages or api requests to return the specific orderitems with toppings prebuilt
+
 COMBO_MESSAGES = [
   'Krabby Meal',
   'Double Krabby Meal',
@@ -51,22 +86,4 @@ COMBO_MESSAGES = [
   'Golden Loaf'
 ]
 
-DRINKS = [
-  'Kelp Shake',
-  'Seafoam Soda'
-]
 
-# Basic Item creation w/ attributes
-
-# Item Attributes:
-# create_table "items", force: :cascade do |t|
-# string "name"
-# decimal "price", precision: 8, scale: 2, default: 0.0
-# integer "item_type"
-
-# Item.create(items)
-
-Topping.create(
-  name: 'Sea Cheese',
-  price: 0.25
-)

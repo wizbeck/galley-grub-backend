05f78b976e1320dc4b739856d44300a9d1e4c6f3
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-09-10 18:08:07 -0600

    [Schema-orders] Add status to orders
    Add status column to orders table, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_121402) do
+ActiveRecord::Schema.define(version: 2022_09_10_230526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_09_08_121402) do
     t.string "cust_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
     t.index ["total"], name: "index_orders_on_total"
   end
 

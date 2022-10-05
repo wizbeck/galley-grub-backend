60ee781101613f05ad101075058657a84373d927
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-10-04 21:32:08 -0600

    OrderItem relationships fix

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,9 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
   has_many :order_item_toppings, dependent: :destroy
+  has_many :item_toppings, through: :order_item_toppings
+  has_many :toppings, through: :item_toppings
 
+  # Nested attributes
   accepts_nested_attributes_for :order_item_toppings
 end

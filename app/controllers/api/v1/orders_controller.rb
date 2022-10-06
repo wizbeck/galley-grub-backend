31908d768031e1c9e21587e8cf355f8eff61a8f6
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-10-06 06:23:25 -0600

    [orders_controller] Update actions to not use instance variables, add opts methods

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,12 +1,31 @@
 class Api::V1::OrdersController < ApplicationController
   def index
     # Retrieve all orders figure out how to incorporate lazy load
-    @orders = Order.order(:created_at).limit(50)
-    render json: OrderSerializer.new(@orders).serializable_hash, status: :ok
+    orders = Order.order(:created_at).limit(50)
+    render json: OrderSerializer.new(orders).serializable_hash, status: :ok
   end
 
   def best_customers
-    @orders = Order.best_customers
-    render json: OrderSerializer.new(@orders).serializable_hash, status: :ok
+    orders = Order.best_customers
+    render json: OrderSerializer.new(orders).serializable_hash, status: :ok
+  end
+
+  def show
+    order = Order.find(params[:order_id])
+    render json: OrderSerializer.new(order, order_opts).serializable_hash, status: :ok
+  end
+
+  private
+
+  def order_opts
+    {
+      include: [:order_items]
+    }
+  end
+
+  def orders_opts
+    {
+      is_collection: true
+    }
   end
 end

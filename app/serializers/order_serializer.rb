fbbdb5e5e7fe619173d24193349c34218a041aad
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-10-13 06:55:07 -0600

    order_items_serializer captures order_items

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,13 +1,14 @@
-class OrderSerializer
+class OrderSerializer < BaseSerializer
   extend ActiveSupport::NumberHelper
   attributes :id, :cust_name
   attribute :total do |order|
     number_to_currency(order.total)
   end
-
-  has_many :order_items, serializer: OrderItemSerializer
+  attribute :order_items do |order|
+    order.order_items.map(&:to_json)
+  end
+  
 
   # Relationship data, similar to activerecord macros
   # specify serializer, to get the proper serialization of records to return in the api request
-
 end

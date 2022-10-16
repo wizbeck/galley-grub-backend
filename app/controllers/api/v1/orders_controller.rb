b6a4fd8822c4a74161ac1dc0fafd269467eb0772
Wesley Beck <74019873+wizbeck21@users.noreply.github.com>
2022-10-15 22:02:07 -0600

    comments plans for orders_controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,15 +1,17 @@
 class Api::V1::OrdersController < ApplicationController
+  # STRETCH
   def index
-    # Retrieve all orders figure out how to incorporate lazy load
     orders = Order.order(:created_at).limit(50)
     render json: OrderSerializer.new(orders, orders_opts).serializable_hash, status: :ok
   end
 
+  # STRETCH -> show orders by highest total amount (best_customers scope)
   def best_customers
     orders = Order.best_customers
     render json: OrderSerializer.new(orders, orders_opts).serializable_hash, status: :ok
   end
 
+  # Shows an order and all its details needed
   def show
     order = Order.find(params[:order_id])
     render json: OrderSerializer.new(order, order_opts).serializable_hash, status: :ok
